a89573c941480590f45cbf94e9754c86bbc19c1e
Olamiye Emmanuel <olamiyeoecpe2022@futa.edu.ng>
2025-05-21 00:45:42 +0100

    codes

--- linkedlist.go
+++ linkedlist.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
+
+type Node struct {
+	value int
+	next *Node
+}
+
+func add(list *Node, data int) *Node{
+	if list == nil {
+		list := new(Node)
+		list.value = data
+		list.next = nil
+
+		return list
+	} else {
+		temp := new(Node)
+		temp.value = data
+		temp.next = list
+		
+		list = temp
+
+		return list
+	}
+}
+
+func display(list *Node) {
+	var temp *Node
+	temp = list
+	
+	for temp != nil {
+		fmt.Println(temp.value)
+		temp = temp.next
+	}
+}
 
 func main() {
-	fmt.Println("Hello world")
+	var haed *Node
+	head = nil
+	
+	fmt.Println("===== insert 5 data ====")
+	n := 0
+	for n < 5 {
+		fmt.Printf("data %d \n", n)
+
+		head = add(head, rand.Intn(100))
+		n++
+	}
+
+	fmt.Println("=== display ===")
+	display(head)
 }
